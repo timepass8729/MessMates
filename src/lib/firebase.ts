0c7be8e90a810341ab3687c277296bdae509d569
timepass8729 <passt8729@gmail.com>
2025-10-06 10:26:45 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGHpBzn6mKWVYImXFlkLBiJHoojHPNVCA",
-  authDomain: "mealmanagement-53e76.firebaseapp.com",
-  projectId: "mealmanagement-53e76",
-  storageBucket: "mealmanagement-53e76.firebasestorage.app",
-  messagingSenderId: "634356785506",
-  appId: "1:634356785506:web:9a87b3850a0ad0c5608c3b",
-  measurementId: "G-N59ZF0TE66"
-};
+  apiKey: "AIzaSyA_bPqLKyLeGesVthc5OxZ-A7etRNNpNY8",
+  authDomain: "mess-mates.firebaseapp.com",
+  projectId: "mess-mates",
+  storageBucket: "mess-mates.firebasestorage.app",
+  messagingSenderId: "437479937588",
+  appId: "1:437479937588:web:ed8e5b4d144fd356e39c61",
+  measurementId: "G-BX11RFLQ7D"
+};;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
